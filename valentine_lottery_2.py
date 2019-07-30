9e43c2c79decb5571af25c01555be947bbb229b4
ryo-simon_mf <ryosimon1108@gmail.com>
2019-07-30 11:59:39 +0900

    first commit

---
+++ valentine_lottery_2.py
@@ -0,0 +1,37 @@
+import random
+
+while number!=7:
+number = random.randint(1,11)
+
+if number == 1:
+  print(" zefさん ")
+
+if number == 2:
+  print(" ゆりあさん ")
+
+if number == 3:
+  print(" Yuri　Ishikawaさん ")
+
+if number == 4:
+  print(" Motoya Tanakaさん ")
+
+if number == 5:
+  print(" れにうむさん ")
+
+if number == 6:
+  print(" ながたしさん ")
+
+if number == 7:
+  print(" 左脳くん ")
+
+if number == 8:
+  print(" Fumiya　Matsunagaさん ")
+
+if number == 9:
+  print(" しーしさん ")
+
+if number == 10:
+  print(" ササキマンチームさん ")
+
+if number == 11:
+  print(" 小林ずずめくん、君はもう時間遅れだ。再抽選です。")
