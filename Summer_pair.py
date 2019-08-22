cc794c4015503ff915c1465a7ab17d5f2c8c23d2
ryo-simon_mf <ryosimon1108@gmail.com>
2019-08-22 13:32:31 +0900

    add summer_pair

---
+++ Summer_pair.py
@@ -0,0 +1,48 @@
+import random
+
+number = random.randint(1,15)
+
+if number == 1:
+  print("Reichon  ")
+
+if number == 2:
+  print("左脳")
+
+if number == 3:
+  print("あきほ.")
+
+if number == 4:
+  print(" かとりく ")
+
+if number == 5:
+  print(" Ami ")
+
+if number == 6:
+  print(" きだっちょ ")
+
+if number == 7:
+  print(" pao ")
+
+if number == 8:
+  print(" みやざわ ")
+
+if number == 9:
+  print(" Furby")
+
+if number == 10:
+  print(" Ami.h ")
+
+if number == 11:
+  print("しゅんた")
+
+if number == 12:
+  print("つはゆうや")
+
+if number == 13:
+  print("俊龍警察")
+
+if number == 14:
+  print("しーし")
+
+if number == 15:
+  print("Mtoret Tanaka")
